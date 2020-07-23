6e16677a5479f7e5ee8921a822d79c46ff617e88
neetuchauhan0421 <neetuchauhan0421@gmail.com>
2020-07-23 18:06:43 +0530

    Updates

---
+++ precache-manifest.bf762e711c1d5dfb9415c806b1366bd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a05b29f6f0d9a5ec8857d83134f05fb0",
+    "url": "/doodleblueTask/index.html"
+  },
+  {
+    "revision": "0111be2d8812e311ecf1",
+    "url": "/doodleblueTask/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3c275dc4c9273fd349ae",
+    "url": "/doodleblueTask/static/js/2.1405206f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/doodleblueTask/static/js/2.1405206f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0111be2d8812e311ecf1",
+    "url": "/doodleblueTask/static/js/main.a79d7b44.chunk.js"
+  },
+  {
+    "revision": "8c71f3a58ce7d5a8aea9",
+    "url": "/doodleblueTask/static/js/runtime-main.1cf04e55.js"
+  }
+]);
